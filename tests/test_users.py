b1a222b5f1408a0f6c385ef61ea6484123cd7eb9
bidyut-sikder <bidyutsikder2001@gmail.com>
2024-12-30 08:23:22 +0600

    adding documentation to the build-deploy.yml file

--- tests/test_users.py
+++ tests/test_users.py
@@ -21,7 +21,7 @@ def test_create_users(client,session):
 def test_get_users(client,test_users):
     res = client.get(f"/users/{test_users['id']}")
 
-    assert res.status_code==201 #changed from 200 to 201
+    assert res.status_code==200
 
 
 
